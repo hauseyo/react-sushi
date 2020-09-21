0fee5667bf6224b1e9ddcd352f0fcf1aee528f07
hauseyo <rosadeyventi@gmail.com>
2020-09-21 23:11:43 +0300

    feat: add logo to header

--- src/ui/organisms/header.tsx
+++ src/ui/organisms/header.tsx
@@ -1,5 +1,14 @@
 import React, { FC } from "react"
+import { NavLink } from "react-router-dom"
 
 export const Header: FC = ({ children }) => {
-  return <header>{children}</header>
+  return (
+    <header style={{ display: "flex", justifyContent: "space-between" }}>
+      <NavLink to="/">
+        <span>React</span>
+        <span>Sushi</span>
+      </NavLink>
+      {children}
+    </header>
+  )
 }
