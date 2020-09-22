a1f1f2cb32ede5c9f9d43e136fd1b16e20589134
hauseyo <rosadeyventi@gmail.com>
2020-09-22 21:37:21 +0300

    feat: create a new page-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react"
-// import { Main } from "pages"
-import { Header } from "ui/organisms"
+import { Route, Switch } from "react-router-dom"
+import { Footer, Header } from "ui"
+import { Navigation } from "features"
+import { Menu } from "pages"
 
 export const App: React.FC = () => {
   return (
     <>
-      <Header>dsf</Header>
+      <Header>
+        <Navigation />
+      </Header>
+      <Switch>
+        <Route path="/" exact={true} component={Menu} />
+      </Switch>
+      <Footer>footer</Footer>
     </>
   )
 }
