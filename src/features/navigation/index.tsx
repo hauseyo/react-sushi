c460ee18bb61a307e2710092ed53938d48a97093
hauseyo <rosadeyventi@gmail.com>
2020-09-22 22:31:55 +0300

    feat: use antd menu to display nav

--- src/features/navigation/index.tsx
+++ src/features/navigation/index.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from "react"
 import { NavLink } from "react-router-dom"
+import { Menu } from "antd"
 
 export const Navigation: FC = () => {
   return (
-    <>
-      <NavLink to="/">Меню</NavLink>
-      <NavLink to="/reviews">Отзывы</NavLink>
-    </>
+    <Menu
+      mode="horizontal"
+      style={{ borderBottom: "none", fontSize: "1.2rem" }}
+    >
+      <Menu.Item>
+        <NavLink to="/">Меню</NavLink>
+      </Menu.Item>
+      <Menu.Item>
+        <NavLink to="/reviews">Отзывы</NavLink>
+      </Menu.Item>
+    </Menu>
   )
 }
