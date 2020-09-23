277b8c1de64db9bfae6d6750337199d2ddaf37fd
hauseyo <rosadeyventi@gmail.com>
2020-09-24 01:04:10 +0300

    feat: move dbs to its folder

--- src/features/card-dishes/card-nigiri/organism/card-nigiri.tsx
+++ src/features/card-dishes/card-nigiri/organism/card-nigiri.tsx
@@ -1,21 +1,46 @@
 import React, { FC } from "react"
+import * as db from "db/nigiri.json"
 import { CardContent, MyCard } from "ui"
 
+type cardInfo = {
+  name: string
+  description: string
+  img: string
+  price: number
+  weight: number
+  protein: number
+  fat: number
+  carb: number
+  cal: number
+}
+
 export const CardNigiri: FC = () => {
+  const data = (db as any).default
+
   return (
     <CardContent>
-      <MyCard
-        src="/nigiri/unagi_new"
-        title="Тунец"
-        price={3.15}
-        description="huge"
-      />
-      <MyCard
-        src="/nigiri/unagi_new"
-        title="Тунец"
-        price={3.15}
-        description="huge"
-      />
+      {data.map(
+        ({
+          name,
+          description,
+          img,
+          price,
+          weight,
+          protein,
+          fat,
+          carb,
+          cal,
+        }: cardInfo) => {
+          return (
+            <MyCard
+              key={name + img}
+              src={`/nigiri/${img}`}
+              title={name}
+              price={price}
+            />
+          )
+        }
+      )}
     </CardContent>
   )
 }
