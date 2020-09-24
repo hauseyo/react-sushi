c6502ff7ed5c2ccb4d823b09edcae46aa5fb18e5
hauseyo <rosadeyventi@gmail.com>
2020-09-24 19:54:20 +0300

    feat: pass prop and onClick

--- src/ui/atoms/button.tsx
+++ src/ui/atoms/button.tsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React, { CSSProperties } from "react"
 import { Button } from "antd"
 
 interface IProps {
   text?: string
   icon?: JSX.Element
+  style?: CSSProperties
+  type?: "primary" | "ghost"
+  onClick: () => void
 }
 
-export const MyButton = ({ text, icon }: IProps) => {
-  return <Button icon={icon}>{text}</Button>
+export const MyButton = ({ text, icon, type, style, onClick }: IProps) => {
+  return (
+    <Button icon={icon} type={type} style={style} onClick={onClick}>
+      {text}
+    </Button>
+  )
 }
