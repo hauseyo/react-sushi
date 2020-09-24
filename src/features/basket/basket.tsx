4f94dc041b1bcdb5ac5b61eea4d5950110e5d341
hauseyo <rosadeyventi@gmail.com>
2020-09-24 19:54:05 +0300

    feat: create modal to display basket

--- src/features/basket/basket.tsx
+++ src/features/basket/basket.tsx
@@ -1,6 +1,41 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
+import { ShoppingCartOutlined } from "@ant-design/icons"
+import { Modal } from "antd"
 import { MyButton } from "ui"
 
 export const Basket: FC = () => {
-  return <MyButton text="Корзина" />
+  const [modalOpened, setModalOpened] = useState<boolean>(false)
+
+  return (
+    <>
+      <MyButton
+        onClick={() => setModalOpened(!modalOpened)}
+        text="23 руб."
+        type="primary"
+        style={{
+          position: "fixed",
+          bottom: "10%",
+          right: "10%",
+          fontSize: "1rem",
+          fontWeight: 600,
+        }}
+        icon={<ShoppingCartOutlined />}
+      />
+      <Modal
+        title="Корзина"
+        visible={modalOpened}
+        footer={[
+          <MyButton
+            key="1"
+            type="primary"
+            onClick={() => setModalOpened(false)}
+            text="Закрыть"
+          />,
+        ]}
+        onCancel={() => setModalOpened(false)}
+      >
+        л
+      </Modal>
+    </>
+  )
 }
