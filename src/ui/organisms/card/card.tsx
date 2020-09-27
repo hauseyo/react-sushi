848eb3b5a8ed6057d7af4a7b8c915234271a79cf
hauseyo <rosadeyventi@gmail.com>
2020-09-27 23:53:06 +0300

    feat: add modal to display all coming props

--- src/ui/organisms/card/card.tsx
+++ src/ui/organisms/card/card.tsx
@@ -1,41 +1,81 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { Card } from "antd"
-import { PlusOutlined, MinusOutlined } from "@ant-design/icons"
+import { PlusOutlined, MinusOutlined, InfoOutlined } from "@ant-design/icons"
 import Meta from "antd/lib/card/Meta"
 import cls from "./style.module.css"
 import { MyButton } from "ui"
+import { MyModal } from "../modal/modal"
+import { MyTitle } from "ui/atoms"
+import { Gist } from "ui/molecules/gist/gist"
 
 interface props {
   src: string
   title: string
+  description: string
   price: number
   quantity?: number
+  weight: number
+  protein: number
+  fat: number
+  carb: number
+  cal: number
 }
 
-export const MyCard: FC<props> = ({ src, title, price, quantity }) => {
+export const MyCard: FC<props> = ({
+  src,
+  title,
+  price,
+  quantity,
+  description,
+  weight,
+  protein,
+  fat,
+  carb,
+  cal,
+}) => {
+  const [modal, setModal] = useState<boolean>(false)
+
   return (
-    <Card
-      className={cls.card}
-      cover={
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/dishes/${src}.png`}
-          alt="sushi"
-        />
-      }
-      actions={[
-        <MyButton icon={<MinusOutlined />} onClick={() => {}} />,
-        <span>{quantity || ""}</span>,
-        <MyButton icon={<PlusOutlined />} onClick={() => {}} />,
-      ]}
-    >
-      <Meta
-        title={title}
-        description={
-          <div className={cls.description}>
-            <span>1 шт.</span> <span>{price} руб.</span>
-          </div>
+    <>
+      <Card
+        className={cls.card}
+        cover={
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/dishes/${src}.png`}
+            alt="sushi"
+            onClick={() => setModal(true)}
+          />
         }
-      />
-    </Card>
+        actions={[
+          <MyButton icon={<MinusOutlined />} onClick={() => {}} />,
+          <span>{quantity || ""}</span>,
+          <MyButton icon={<PlusOutlined />} onClick={() => {}} />,
+          <MyButton icon={<InfoOutlined />} onClick={() => setModal(true)} />,
+        ]}
+      >
+        <Meta
+          title={title}
+          description={
+            <div className={cls.description}>
+              <span>1 шт.</span> <span>{price} руб.</span>
+            </div>
+          }
+        />
+      </Card>
+      <MyModal
+        title="Дополнительная информация"
+        visible={modal}
+        onCancel={() => setModal(false)}
+      >
+        <MyTitle text={title} level={3} />
+        <Gist gist="Состав" value={description} />
+        <Gist gist="Вес" value={`${weight} г.`} />
+        <Gist gist="Белки" value={`${protein} г.`} />
+        <Gist gist="Жиры" value={`${fat} г.`} />
+        <Gist gist="Углеводы" value={`${carb} г.`} />
+        <Gist gist="Калории" value={`${cal} кал.`} />
+        <Gist gist="Цена" value={`${price} руб.`} />
+      </MyModal>
+    </>
   )
 }
