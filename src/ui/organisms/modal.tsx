5b5cb77cc8eedf202f1a4ef7db66bf21d9523c2b
hauseyo <rosadeyventi@gmail.com>
2020-09-30 20:34:52 +0300

    feat: pass width through props

--- src/ui/organisms/modal.tsx
+++ src/ui/organisms/modal.tsx
@@ -4,17 +4,25 @@ import { MyButton } from "ui"
 
 type Props = {
   title: string | JSX.Element
+  width?: number
   visible: boolean
   onCancel: () => void
   children: JSX.Element[] | JSX.Element | (JSX.Element[] | JSX.Element)[]
 }
 
-export const MyModal: FC<Props> = ({ title, visible, onCancel, children }) => {
+export const MyModal: FC<Props> = ({
+  title,
+  visible,
+  onCancel,
+  width,
+  children,
+}) => {
   return (
     <Modal
       title={title}
       visible={visible}
       onCancel={onCancel}
+      width={width}
       footer={[
         <MyButton key="1" text="Закрыть" onClick={onCancel} type="primary" />,
       ]}
