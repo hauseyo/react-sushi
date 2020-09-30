2ac52dc5761f22406abf7a7dd429a0dcb217c8b9
hauseyo <rosadeyventi@gmail.com>
2020-09-30 23:24:57 +0300

    feat: add some content to footer

--- src/ui/organisms/footer.tsx
+++ src/ui/organisms/footer.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react"
-import { Layout } from "antd"
+import { Gist } from "ui/molecules"
 
-export const Footer: FC = ({ children }) => {
-  const { Footer } = Layout
-  return <Footer>{children}</Footer>
+export const Footer: FC = () => {
+  return (
+    <footer style={{ maxWidth: 1200, margin: "0 auto", marginTop: 30 }}>
+      <Gist gist={<i className="fas fa-user-edit" />} value="ХО" />
+      <Gist gist={<i className="fab fa-telegram-plane" />} value="@hauseyo" />
+      <Gist gist={<i className="fas fa-phone" />} value="+375 44 455 09 23" />
+      <Gist
+        gist={<i className="fas fa-at" />}
+        value="ho.thachngu3n@gmail.com"
+      />
+    </footer>
+  )
 }
