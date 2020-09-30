41c3c3a2da44c1059a52aa74d109ea67bb802132
hauseyo <rosadeyventi@gmail.com>
2020-09-30 23:25:24 +0300

    fix: gist can be jsx element

--- src/ui/molecules/gist.tsx
+++ src/ui/molecules/gist.tsx
@@ -1,16 +1,16 @@
 import React, { FC } from "react"
 
 type Props = {
-  gist: string
+  gist: string | JSX.Element
   value: string
 }
 
 export const Gist: FC<Props> = ({ gist, value }) => {
   return (
     <p>
-      <span
-        style={{ color: "#EC5450", fontWeight: "bold" }}
-      >{`${gist}: `}</span>
+      <span style={{ color: "#EC5450", fontWeight: "bold", marginRight: 10 }}>
+        {gist}:&nbsp;
+      </span>
       <b>{value}</b>
     </p>
   )
