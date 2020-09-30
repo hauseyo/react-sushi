1d1bf6df468be8c65e2e8c72a9a87b9cd2657793
hauseyo <rosadeyventi@gmail.com>
2020-09-30 22:26:53 +0300

    feat: create a new segment for gunkani

--- src/pages/menu/menu.tsx
+++ src/pages/menu/menu.tsx
@@ -3,8 +3,10 @@ import { Tabs } from "antd"
 import { TabTitle } from "ui"
 import { Cards } from "features/card-dishes"
 import * as nigiriDB from "db/nigiri.json"
+import * as gunkaniDB from "db/gunkani.json"
 
 const nigiri = (nigiriDB as any).default
+const gunkani = (gunkaniDB as any).default
 
 const { TabPane } = Tabs
 
@@ -15,7 +17,7 @@ export const Menu: FC = () => {
         <Cards db={nigiri} />
       </TabPane>
       <TabPane tab={<TabTitle src="gunkani" title="Гунканы" />} key="2">
-        kj
+        <Cards db={gunkani} />
       </TabPane>
       <TabPane tab={<TabTitle src="maki" title="Маки" />} key="3">
         kj
