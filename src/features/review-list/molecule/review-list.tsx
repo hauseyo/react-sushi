80420f3a7331ce6405fe7605fba85003bfef1a2d
hauseyo <rosadeyventi@gmail.com>
2020-10-02 01:28:47 +0300

    feat: display received review list

--- src/features/review-list/molecule/review-list.tsx
+++ src/features/review-list/molecule/review-list.tsx
@@ -1,11 +1,28 @@
 import React, { FC, useEffect } from "react"
-// import { Comment } from "antd"
-import { fetchListFx } from "../model/list"
+import { useStore } from "effector-react"
+import { Comment } from "antd"
+import { fetchListFx, $reviewList } from "../model/list"
+import cls from "./style.module.css"
 
 export const ReviewList: FC = () => {
+  const list = useStore($reviewList)
+
   useEffect(() => {
     fetchListFx()
   }, [])
 
-  return <>{/* <Comment /> */}</>
+  return (
+    <>
+      {Object.keys(list)?.map(key => {
+        const { username, area, date } = list[key]
+        return (
+          <Comment
+            author={<span className={cls.author}>{username}</span>}
+            content={area}
+            datetime={<span className={cls.date}>{date}</span>}
+          />
+        )
+      })}
+    </>
+  )
 }
